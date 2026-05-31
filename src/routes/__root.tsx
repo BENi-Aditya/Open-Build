fbc4dc24048e4a2042f686edf35227f9e3ae3464
Aditya <aditya.tripathi.beni@gmail.com>
2026-05-31 13:55:10 +0530

    google analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -122,6 +122,16 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MWNRN5192Z"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MWNRN5192Z');
+          `
+        }} />
       </head>
       <body>
         {children}
