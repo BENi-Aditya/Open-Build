8dce8cf7f7c5c3ffe3a54e5dd091359d1eb4a367
Aditya <aditya.tripathi.beni@gmail.com>
2026-05-31 16:35:33 +0530

    analytics please

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -122,6 +122,13 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DVPCNK33CG-MWNRN5192Z"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6DVPCNK33C');
+        </script>
       </head>
       <body>
         {children}
